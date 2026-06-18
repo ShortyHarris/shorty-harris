2aea48e3d91ed909e5764bf25848b6baefe65165
ShortyHarris <martin@shortyharris.com>
2026-06-18 12:31:44 +0200

    Update supabase.ts

--- app/src/lib/supabase.ts
+++ app/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = import.meta.env.VITE_SUPABASE_URL as string;
-const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const url = 'https://lxoeotyibsalbxgbjfxo.supabase.co';
+const anonKey = 'sb_publishable_CdK80AB1j_99hPPZGGS_jA_XYsNzrUs';
 
 export const supabase = createClient(url, anonKey, {
   auth: { persistSession: true, autoRefreshToken: true },
